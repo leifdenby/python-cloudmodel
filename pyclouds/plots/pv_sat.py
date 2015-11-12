200724182671ac168506626fddd64ccd53139fb3
Leif Denby <leif@denby.eu>
2015-11-12 10:40:42 +0100

    Fixes to parameterisation plots

--- pyclouds/plots/pv_sat.py
+++ pyclouds/plots/pv_sat.py
@@ -5,18 +5,20 @@ from scipy import optimize
 from pyclouds import parameterisations
 from pyclouds import common
 
-plot.ion()
+savefig = False
+
+plot_kwargs = {}
+if savefig:
+    plot.ion()
+    plot_kwargs['figsize'] = (16, 30)
 
 T_c = np.linspace(-30., 20., 50)
-p = 101325.0
-
-
 
 pv_sat__atham = parameterisations.ParametersationsWithSpecificConstants(common.ATHAM_constants).pv_sat
 pv_sat__ccfm = parameterisations.ParametersationsWithSpecificConstants(common.CCFM_constants).pv_sat
 pv_sat__default = parameterisations.ParametersationsWithSpecificConstants(common.default_constants).pv_sat
 
-plot.figure(figsize=(16,30))
+plot.figure(**plot_kwargs)
 
 plot.subplot(211)
 for model in [pv_sat__atham, pv_sat__ccfm, pv_sat__default]:
@@ -28,12 +30,19 @@ plot.grid(True)
 plot.title("Saturation vapour pressure with different model constants")
 
 plot.subplot(212)
+p = 88676. #101325.0
 for model in [pv_sat__atham, pv_sat__ccfm, pv_sat__default]:
     plot.plot(T_c, model.qv_sat(T=T_c+273.15, p=p), label=str(model), marker='', linestyle=':')
 plot.xlabel("Temperature [C]")
-plot.ylabel("Saturation vapour pressure [Pa]")
+plot.ylabel("Saturation specific water concentration [kg/kg]")
 plot.legend(loc='upper left')
+plot.title("Ambient pressure p=%gPa" % p)
 plot.grid(True)
-plot.title("Saturation specific water vapour concentration with different model constants\nAt P=%gPa" % p)
 
-plot.savefig("pv_sat__parameterisation.png")
+plot.suptitle("Saturation specific water vapour concentration with different model constants")
+
+if savefig:
+    plot.savefig("pv_sat__parameterisation.png")
+else:
+    plot.draw()
+    plot.show()
